a4877001c58a4d5db1c0607f6cc9979532b815aa
DevIcaro16 <icaroip15@gmail.com>
2025-08-24 12:43:37 -0300

    first commit

---
+++ Services/TestService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Devicaro16\MicroservicesCommon\Services;
+
+use Devicaro16\MicroservicesCommon\Services\Traits\ConsumeExternalService;
+
+class TestService
+{
+
+    use ConsumeExternalService;
+
+    public function __construct()
+    {
+
+    }
+
+}
---
+++ Services/Traits/ConsumeExternalService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Devicaro16\MicroservicesCommon\Services\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+trait ConsumeExternalService
+{
+
+    public function headers(array $headers = [])
+    {
+        array_push($headers, [
+            'Accept' => 'application/json',
+            'Authorization' => $this->token
+        ]);
+
+        return Http::withHeaders($headers);
+    }
+
+
+    public function request(
+        string $method,
+        string $endpoint,
+        array $formParams = [],
+        array $headers = [],
+    ) {
+
+        return $this->headers($headers)->$method($this->url . $endpoint, $formParams);
+    }
+
+}
+
+?>
